e527cb756070f9e57f361b8099be230620c40a56
Todd Hambley <thambley@travelleaders.com>
2017-11-22 12:25:32 -0500

    fix typos and formatting

--- lib/active_admin/xls/dsl.rb
+++ lib/active_admin/xls/dsl.rb
@@ -1,12 +1,23 @@
 module ActiveAdmin
   module Xls
+    # extends activeadmin dsl to include xls
     module DSL
-      delegate(:ingnore_columns, :column, :after_filer, :i18n_scope, :header_style, :skip_header, :white_list,
+      delegate(:ignore_columns,
+               :column,
+               :after_filter,
+               :i18n_scope,
+               :header_format,
+               :skip_header,
+               :whitelist,
                to: :xls_builder,
                prefix: :config)
 
       def xls(options = {}, &block)
-        config.xls_builder = ActiveAdmin::Xls::Builder.new(config.resource_class, options, &block)
+        config.xls_builder = ActiveAdmin::Xls::Builder.new(
+          config.resource_class,
+          options,
+          &block
+        )
       end
     end
   end
