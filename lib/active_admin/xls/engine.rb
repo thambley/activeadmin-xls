5300ab895f4e72891ea2b71be7c2de7bbb9dc41d
Todd Hambley <thambley@travelleaders.com>
2017-11-22 12:25:57 -0500

    formatting

--- lib/active_admin/xls/engine.rb
+++ lib/active_admin/xls/engine.rb
@@ -1,11 +1,12 @@
 module ActiveAdmin
   module Xls
+    # extends activeadmin with xls downloads
     class Engine < ::Rails::Engine
       engine_name 'active_admin_xls'
 
-      initializer 'active_admin.xls', :group => :all do |app|
+      initializer 'active_admin.xls', group: :all do
         if Mime::Type.lookup_by_extension(:xls).nil?
-          Mime::Type.register "application/vnd.ms-excel", :xls
+          Mime::Type.register 'application/vnd.ms-excel', :xls
         end
 
         ActiveAdmin::Views::PaginatedCollection.add_format :xls
