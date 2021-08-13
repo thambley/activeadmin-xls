d67442370530e36b3d82e40e6bb779dd47c151c5
Stéphane Hanser <shanser@captive.fr>
2021-08-13 12:58:40 +0200

    Extend ActiveAdmin the before_load way instead of within the engines
    Using the engine causes Rails deprecation warnings because of Autoloading

--- lib/active_admin/xls/engine.rb
+++ lib/active_admin/xls/engine.rb
@@ -8,15 +8,6 @@ module ActiveAdmin
         if Mime::Type.lookup_by_extension(:xls).nil?
           Mime::Type.register 'application/vnd.ms-excel', :xls
         end
-
-        ActiveAdmin::Views::PaginatedCollection.add_format :xls
-
-        ActiveAdmin::ResourceDSL.send :include, ActiveAdmin::Xls::DSL
-        ActiveAdmin::Resource.send :include, ActiveAdmin::Xls::ResourceExtension
-        ActiveAdmin::ResourceController.send(
-          :prepend,
-          ActiveAdmin::Xls::ResourceControllerExtension
-        )
       end
     end
   end
